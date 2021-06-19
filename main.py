20c721a10df8d22f987abf6fe3d82bf03cfdc700
monicamourad <monicamourad18@hotmail.com>
2021-06-19 20:06:27 +0200

    trying pytorch

--- main.py
+++ main.py
@@ -1,9 +1,62 @@
 import numpy as np
 import pandas as pd
-from keras.models import load_model
-from sklearn.model_selection import train_test_split
-from keras.layers import Input, Embedding, Flatten, Dot, Dense, Concatenate
-from keras.models import Model
+from sklearn import model_selection
+from sklearn import metrics
+import torch
+import torch.nn as nn
+import tez
+
+class ProductDataset:
+    def __init__(self,users,products,event_weights):
+        self.users=users
+        self.products=products
+        self.event_weights=event_weights
+
+    def __len__(self):
+        return len(self.users)
+
+    def __getitem__(self,item):
+        user=self.users[item]
+        product=self.products[item]
+        event_weight=self.event_weights[item]
+
+        return{
+            'user': torch.tensor(user,dtype=torch.long),
+            'product':torch.tensor(product,dtype=torch.long),
+            'event_weight':torch.tensor(event_weight,dtype=torch.float),
+        }
+
+class RecSysModel(tez.Model):
+    def __init__(self,num_users,num_products):
+        super().__init__()
+        self.user_embed = nn.Embedding(num_users,32)
+        self.product_embed = nn.Embedding(num_products,32)
+        self.out = nn.Linear(64,1)
+        self.step_scheduler_after = "epoch"
+
+    def fetch_optimizer(self):
+        opt = torch.optim.Adam(self.parameters(),lr = 1e-3)
+        return opt
+
+    def fetch_scheduler(self):
+        sch = torch.optim.lr_scheduler.StepLR(self.optimizer,step_size=3,gamma=0.7)
+        return sch
+
+    def monitor_metrics(self, output, event_weight):
+        output = output.detach().cpu().numpy()
+        event_weight = event_weight.detach().cpu().numpy()
+        return {"rmse" : np.sqrt(metrics.mean_squared_error(event_weight,output))}
+
+    def forward(self,users,products,event_weights = None):
+        print("forward")
+        user_embeds = self.user_embed(users)
+        product_embeds = self.product_embed(products)
+        output = torch.cat([user_embeds,product_embeds],dim=1)
+        output = self.out(output)
+
+        loss = nn.MSELoss()(output,event_weights.view(-1,1))
+        calc_metrics = self.monitor_metrics(output,event_weights.view(-1,1))
+        return output , loss , calc_metrics
 
 
 def pre_processing():
@@ -35,43 +88,19 @@ def checking_data():
 def event_strength_to_rating():
     print("\nevent_strength_to_rating is not implemented yet\n")
 
-def shallow_learning():
-    df_processed = pd.read_csv("Cleaned-2019-Oct.csv") 
-    n_items = df_processed['product_id'].nunique()
-    n_users = df_processed['user_id'].nunique()
-    train, test = train_test_split(df_processed, test_size=0.2, random_state=42)
-
-    # creating book embedding path
-    product_input = Input(shape=[1], name="Product-Input")
-    product_embedding = Embedding(n_items+1, 5, name="Product-Embedding")(product_input)
-    product_vec = Flatten(name="Flatten-Products")(product_embedding)
-
-    # creating user embedding path
-    user_input = Input(shape=[1], name="User-Input")
-    user_embedding = Embedding(n_users+1, 5, name="User-Embedding")(user_input)
-    user_vec = Flatten(name="Flatten-Users")(user_embedding)
-
-    # concatenate features
-    conc = Concatenate()([product_vec, user_vec])
-    # add fully-connected-layers
-    fc1 = Dense(128, activation='relu')(conc)
-    fc2 = Dense(32, activation='relu')(fc1)
-    out = Dense(1)(fc2)
-
-    # Create model and compile it
-    model2 = Model([user_input, product_input], out)
-    model2.compile('adam', 'mean_squared_error')
-
-    history = model2.fit([train.user_id, train.product_id], train.event_strength, epochs=5, verbose=1)
-    predictions = model2.predict([test.user_id.head(10), test.product_id.head(10)])
-    [print(predictions[i], test.event_strength.iloc[i]) for i in range(0,10)]
+def train():
+    df = pd.read_csv("Cleaned-2019-Oct.csv") #user_id,product_id,event_strength
+    df_train,df_test = model_selection.train_test_split(df,test_size=0.1,random_state=42)
+    train_dataset = ProductDataset( users = df_train.user_id.values, products = df_train.product_id.values, event_weights = df_train.event_strength.values)
+    test_dataset = ProductDataset( users = df_test.user_id.values, products = df_test.product_id.values, event_weights = df_test.event_strength.values)
+    model = RecSysModel(num_users = df['user_id'].nunique(), num_products = df['product_id'].nunique())
+    model.fit(train_dataset, test_dataset, valid_bs = 16, fp16= True,device="cpu")
 
 def main():
    # pre_processing()
    # checking_data()
     event_strength_to_rating()
-    print("\nData is cleaned\n")
-    shallow_learning()
+    train()
 
 if __name__ == "__main__":
 	main()
