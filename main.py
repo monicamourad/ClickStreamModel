7a9599459c7d65ae0887e8a4ae4f311040de6e43
monicamourad <monicamourad18@hotmail.com>
2021-07-12 19:39:56 +0200

    fixed error for our data

--- main.py
+++ main.py
@@ -1,188 +1,174 @@
 import numpy as np
 import pandas as pd
-from sklearn import model_selection
-from sklearn import metrics , preprocessing
-import torch
-import torch.nn as nn
-import tez
 import implicit
 from scipy.sparse import csr_matrix
 import pickle
 import random
 from sklearn.preprocessing import MinMaxScaler
-
-class ProductDataset:
-    def __init__(self,users,products,event_weights):
-        self.users=users
-        self.products=products
-        self.event_weights=event_weights
-
-    def __len__(self):
-        return len(self.users)
-
-    def __getitem__(self,item):
-        user=self.users[item]
-        product=self.products[item]
-        event_weight=self.event_weights[item]
-
-        return{
-            'users': torch.tensor(user,dtype=torch.long),
-            'products':torch.tensor(product,dtype=torch.long),
-            'event_weights':torch.tensor(event_weight,dtype=torch.float),
-        }
-
-class RecSysModel(tez.Model):
-    def __init__(self,num_users,num_products):
-        super().__init__()
-        self.user_embed = nn.Embedding(num_users,32)
-        self.product_embed = nn.Embedding(num_products,32)
-        self.out = nn.Linear(64,1)
-        self.step_scheduler_after = "epoch"
-
-    def fetch_optimizer(self):
-        opt = torch.optim.Adam(self.parameters(),lr = 1e-3)
-        return opt
-
-    def fetch_scheduler(self):
-        sch = torch.optim.lr_scheduler.StepLR(self.optimizer,step_size=3,gamma=0.7)
-        return sch
-
-    def monitor_metrics(self, output, event_weight):
-        output = output.detach().cpu().numpy()
-        event_weight = event_weight.detach().cpu().numpy()
-        return {"rmse" : np.sqrt(metrics.mean_squared_error(event_weight,output)) }
-
-    def forward(self,users,products,event_weights = None):
-        user_embeds = self.user_embed(users)
-        product_embeds = self.product_embed(products)
-        output = torch.cat([user_embeds,product_embeds],dim=1)
-        output = self.out(output)
-
-        loss = nn.MSELoss(output,event_weights.view(-1,1))
-        calc_metrics = self.monitor_metrics(output,event_weights.view(-1,1))
-        return output , loss , calc_metrics
+from sklearn import metrics
 
 def pre_processing():
-#     event_type_strength = {
-#    'view': 1.0,
-#    'cart': 2.0, 
-#    'purchase': 3.0, 
-#    'remove_from_cart': -1.0,}
-#     df = pd.read_csv("2019-Oct.csv")
-#     df.drop_duplicates(inplace=True)
-#     df.dropna(inplace=True)
-#     df.drop(axis=0,columns=['event_time','price','category_id','brand','user_session'],inplace=True)
-#     df['event_strength'] = df['event_type'].apply(lambda x: event_type_strength[x])
-    #df.to_csv("semi-Cleaned-2019-Oct.csv",index=False)
-    df = pd.read_csv("semi-Cleaned-2019-Oct.csv")
+
+    event_type_strength = {
+   'view': 1.0,
+   'cart': 2.0, 
+   'purchase': 3.0, 
+   'remove_from_cart': -1.0,}
+
+    df = pd.read_csv("2019-Oct.csv")
+
+    df.drop(axis=0,columns=['event_time','price','category_id','brand','user_session'],inplace=True)
+    df.dropna(inplace=True)
     df.drop_duplicates(inplace=True)
+    df['event_strength'] = df['event_type'].apply(lambda x: event_type_strength[x])
+
+    df.to_csv("semi-Cleaned-2019-Oct.csv",index=False)
+
+    #df = pd.read_csv("semi-Cleaned-2019-Oct.csv")
+
     df.drop(axis=0,columns=['event_type'],inplace=True)
-    print(df[df['user_id'] == 240522111])
     grouped_df = df.groupby(['user_id', 'product_id','category_code'], as_index=False)['event_strength'].sum()
+
     grouped_df.to_csv("Cleaned-2019-Oct.csv",index=False)
+    print("Exiting pre_processing")
 
 def checking_data():
     #df1 = pd.read_csv("semi-Cleaned-2019-Oct.csv")
-    #df1 = df1.query('user_id == 517005004 & product_id == 5100566')
     #print(df1)
-    df2 = pd.read_csv("Cleaned-2019-Oct.csv")
-    print(df2.head())
-    print(df2['event_strength'].value_counts())
-    print(df2[df2['user_id'] == 240522111])
-
-    #print(df2['product_id'].nunique())
-    #df2 = df2.query('user_id == 517005004 & product_id == 5100566')
-    # print(df2.query('event_strength == 1483.0'))
-    # print("MAX",df2['event_strength'].max())
-    # print("MIN",df2['event_strength'].min())
-
-def event_strength_to_rating():
-    print("\nevent_strength_to_rating is not implemented yet\n")
-
-def train():
-    df = pd.read_csv("Cleaned-2019-Oct.csv") #user_id,product_id,event_strength
-    lbl_user = preprocessing.LabelEncoder()
-    lbl_product = preprocessing.LabelEncoder()
-    df.user_id = lbl_user.fit_transform(df.user_id.values)
-    df.product_id = lbl_product.fit_transform(df.product_id.values)
-
-    df_train,df_test = model_selection.train_test_split(df,test_size=0.05,random_state=42)
-    df_train,df_develop = model_selection.train_test_split(df,test_size=0.05,random_state=42)
-
-    train_set = ProductDataset( users = df_train.user_id.values, products = df_train.product_id.values, event_weights = df_train.event_strength.values)
-    devlopment_set = ProductDataset( users = df_develop.user_id.values, products = df_develop.product_id.values, event_weights = df_develop.event_strength.values)
-    test_set = ProductDataset( users = df_test.user_id.values, products = df_test.product_id.values, event_weights = df_test.event_strength.values)
-    model = RecSysModel(num_users = len(lbl_user.classes_), num_products = len(lbl_product.classes_))
-    model.fit(train_set, devlopment_set, epochs=1 ,train_bs=512,valid_bs = 512, fp16= True,device="cpu")
+    test_df = pd.read_csv("Cleaned-2019-Oct.csv")
+    print(test_df.head())
+    print(test_df['event_strength'].value_counts())
+   # print(df2[df2['user_id'] == 240522111])
+    print("MAX",test_df['event_strength'].max())
+    print("MIN",test_df['event_strength'].min())
+    print("Exiting checking_data")
 
 def Als():
     grouped_df = pd.read_csv("Cleaned-2019-Oct.csv")
-    # Normalizing values
+
+    # Normalizing column values
     grouped_df['category_code'] = grouped_df['category_code'].astype("category")
     grouped_df['user_id'] = grouped_df['user_id'].astype("category")
     grouped_df['product_id'] = grouped_df['product_id'].astype("category")
     grouped_df['user_id'] = grouped_df['user_id'].cat.codes
     grouped_df['product_id'] = grouped_df['product_id'].cat.codes
+    # #---------------------------------------------TEST----------------------------------------------------------
+    # print("min user_id",grouped_df['user_id'].min())
+    # print("max user_id",grouped_df['user_id'].max())
+    # print("min product_id",grouped_df['product_id'].min())
+    # print("max product_id",grouped_df['product_id'].max())
+    # print("number of unique user_id",grouped_df.user_id.nunique())
+    # print("number of unique product_id",grouped_df.product_id.nunique())
+    # #-----------------------------------------------------------------------------------------------------------
 
-    sparse_content_person = csr_matrix((grouped_df['event_strength'].astype(float), (grouped_df['product_id'], grouped_df['user_id'])))
-    sparse_person_content = csr_matrix((grouped_df['event_strength'].astype(float), (grouped_df['user_id'], grouped_df['product_id'])))
+    # csr_matrix((data, (row_ind, col_ind)), [shape=(M, N)])
+    # where data, row_ind and col_ind satisfy the relationship a[row_ind[k], col_ind[k]] = data[k].
 
-    model = implicit.als.AlternatingLeastSquares(factors=20, regularization=0.1, iterations=50)
+    #user-based callaborative filtering
+    sparse_product_user = csr_matrix((grouped_df['event_strength'].astype(float), (grouped_df['product_id'], grouped_df['user_id'])))
+    #item-based callaborative filtering
+    sparse_user_product = csr_matrix((grouped_df['event_strength'].astype(float), (grouped_df['user_id'], grouped_df['product_id'])))
+
+    model = implicit.als.AlternatingLeastSquares(factors=20, regularization=0.1, iterations=50,calculate_training_loss=True)
 
     alpha = 15
-    data = (sparse_content_person * alpha).astype('double')
+    data = (sparse_product_user * alpha).astype('double')
     model.fit(data)
 
     with open('model.pkl', 'wb') as handle:
         pickle.dump(model, handle)
-    with open('sparse_person_content.pkl', 'wb') as handle:
-        pickle.dump(sparse_person_content, handle)
+    with open('sparse_user_product.pkl', 'wb') as handle:
+        pickle.dump(sparse_user_product, handle)
     grouped_df.to_csv("Als.csv",index=False)
+    print("Exiting Als")
 
 def find_similar_products(content_id,n_similar):
+
     grouped_df = pd.read_csv("Als.csv")
-    print(grouped_df[grouped_df['category_code']=='kids.carriage'])
+    print("Original category_code \n",grouped_df[grouped_df['product_id']==content_id][:1])
+
     with open('model.pkl', 'rb') as handle:
         model = pickle.load(handle)
 
-    person_vecs = model.user_factors
-    content_vecs = model.item_factors
+    product_vecs = model.item_factors #2D array[number of unique product_id,factors]
+    # #---------------------------------------------TEST----------------------------------------------------------
+    # print("\nnumber of unique product_id",grouped_df.product_id.nunique())
+    # print("product latent factors\n",len(product_vecs),len(product_vecs[0]))
+    # #-----------------------------------------------------------------------------------------------------------
 
-    content_norms = np.sqrt((content_vecs * content_vecs).sum(axis=1))
 
-    scores = content_vecs.dot(content_vecs[content_id]) / content_norms
+    ##Calculate the vector norms
+    #element wise multiplication and sqrt
+    product_norms = np.sqrt((product_vecs * product_vecs).sum(axis=1))
+    # #---------------------------------------------TEST----------------------------------------------------------
+    # test_product_vecs = np.array([[2,0],[3,8],[6,8]])
+    # test_product_norms = np.sqrt((test_product_vecs * test_product_vecs).sum(axis=1))
+    # print("\nTesting product norm",test_product_norms)
+    # #-----------------------------------------------------------------------------------------------------------
+
+
+    #Calculate the similarity score
+    scores = product_vecs.dot(product_vecs[content_id]) / product_norms
+    # #---------------------------------------------TEST----------------------------------------------------------
+    # test_scores = test_product_vecs.dot(test_product_vecs[1]) / test_product_norms
+    # print("\nTesting dot products",test_scores,"\n")
+    # #-----------------------------------------------------------------------------------------------------------
+
+
+    #Get the top 10 PRODUCTS
     top_idx = np.argpartition(scores, -n_similar)[-n_similar:]
-    similar = sorted(zip(top_idx, scores[top_idx] / content_norms[content_id]), key=lambda x: -x[1])
+    # #---------------------------------------------TEST----------------------------------------------------------
+    # test_top_idx = np.argpartition(test_scores, -2)[-2:]
+    # print("\ntest_top_idx",test_top_idx,"\n")
+    # #-----------------------------------------------------------------------------------------------------------
 
+
+    #Create a list of content-score tuples of most similar products to this product.
+    similar = sorted(zip(top_idx, scores[top_idx] / product_norms[content_id]), key=lambda x: -x[1])
     for content in similar:
         idx, score = content
-        print(grouped_df.category_code.loc[grouped_df.product_id == idx].iloc[0])
+        print(grouped_df.category_code.loc[grouped_df.product_id == idx].iloc[0],score)
+    # #---------------------------------------------TEST----------------------------------------------------------
+    # test_similar = sorted(zip(test_top_idx, test_scores[test_top_idx] / test_product_norms[1]), key=lambda x: -x[1])
+    # print("\ntest_similar",test_similar)
+    # #-----------------------------------------------------------------------------------------------------------
 
+    print("Exiting find_similar_products\n")
 
 def recommend(person_id, num_contents=10):
+    grouped_df = pd.read_csv("Als.csv")
     with open('model.pkl', 'rb') as handle:
             model = pickle.load(handle)
-    with open('sparse_person_content.pkl', 'rb') as handle:
-        sparse_person_content = pickle.load(handle)
+    with open('sparse_user_product.pkl', 'rb') as handle:
+        sparse_user_product = pickle.load(handle)
 
-     # Get the trained person and content vectors. We convert them to csr matrices
-    person_vecs = csr_matrix(model.user_factors)
-    content_vecs = csr_matrix(model.item_factors)
-    grouped_df = pd.read_csv("Als.csv")
+        #explain(userid, user_items, itemid, user_weights=None, N=10)
 
-    # Get the interactions scores from the sparse person content matrix
-    person_interactions = sparse_person_content[person_id,:].toarray()
-    # Add 1 to everything, so that articles with no interaction yet become equal to 1
-    person_interactions = person_interactions.reshape(-1) + 1
-    # Make articles already interacted zero
-    person_interactions[person_interactions > 1] = 0
-    # Get dot product of person vector and all content vectors
-    rec_vector = person_vecs[person_id,:].dot(content_vecs.T).toarray()
+    # Extract user_vecs and product_vecs from trained mode
+    user_vecs = model.item_factors #2D array[number of unique user_id,factors]
+    product_vecs = model.item_factors #2D array[number of unique product_id,factors]
+
+    # Convert user_vecs and product_vecs to csr matrices
+    user_vecs = csr_matrix(user_vecs)
+    product_vecs = csr_matrix(product_vecs)
+
+    # Get the interactions scores from the sparse_user_product matrix
+    user_interactions = sparse_user_product[person_id,:].toarray()
+    #print("Test",user_interactions[user_interactions > 0])
+    # Add 1 to everything, so that products with no interaction yet become equal to 1
+    user_interactions = user_interactions.reshape(-1) + 1
+    #print("Test",user_interactions[user_interactions > 1 ])
+    # Make products already interacted with zero
+    user_interactions[user_interactions > 1] = 0
+    #print("Test",user_interactions[user_interactions > 1])
+
+    # Get dot product of user vector and all product vectors
+    rec_vector = user_vecs[person_id,:].dot(product_vecs.T).toarray()
     # Scale this recommendation vector between 0 and 1
     min_max = MinMaxScaler()
     rec_vector_scaled = min_max.fit_transform(rec_vector.reshape(-1,1))[:,0]
     # Content already interacted have their recommendation multiplied by zero
-    recommend_vector = person_interactions * rec_vector_scaled
+    recommend_vector = user_interactions * rec_vector_scaled
     # Sort the indices of the content into order of best recommendations
     content_idx = np.argsort(recommend_vector)[::-1][:num_contents]
     
@@ -200,9 +186,11 @@ def recommend(person_id, num_contents=10):
     print(recommendations) 
     print("=================================================================================") 
     print(grouped_df[grouped_df['user_id'] == person_id].sort_values(by=['event_strength'], ascending=False).head())
+    print("Exiting recommend\n")
+
    
 def make_train(pct_test = 0.2):
-    with open('sparse_person_content.pkl', 'rb') as handle:
+    with open('sparse_user_product.pkl', 'rb') as handle:
         ratings = pickle.load(handle)
 
     test_set = ratings.copy() # Make a copy of the original set to be the test set. 
@@ -227,6 +215,7 @@ def make_train(pct_test = 0.2):
     training_set[content_inds, person_inds] = 0 # Assign all of the randomly chosen user-item pairs to zero
     training_set.eliminate_zeros() # Get rid of zeros in sparse array storage after update to save space
     
+    print("Exiting make_train\n")
     return training_set, test_set, list(set(person_inds))  
 
 def auc_score(predictions, test):
@@ -236,23 +225,23 @@ def auc_score(predictions, test):
 def calc_mean_auc(training_set, altered_persons, test_set):
     with open('model.pkl', 'rb') as handle:
             model = pickle.load(handle)
-    person_vecs = csr_matrix(model.user_factors)
+    user_vecs = csr_matrix(model.user_factors)
     content_vecs = csr_matrix(model.item_factors)
-    predictions = [person_vecs, content_vecs.T]
+    predictions = [user_vecs, content_vecs.T]
     store_auc = [] # An empty list to store the AUC for each user that had an item removed from the training set
     popularity_auc = [] # To store popular AUC scores
     pop_contents = np.array(test_set.sum(axis = 1)).reshape(-1) # Get sum of item iteractions to find most popular
     content_vecs = predictions[1]
     for person in altered_persons: # Iterate through each user that had an item altered
-        training_column = training_set[:,person].toarray().reshape(-1) # Get the training set column
+        #################################################################################################
+        training_column = training_set[person,:].toarray().reshape(-1) # Get the training set column
+        #################################################################################################
+
         zero_inds = np.where(training_column == 0) # Find where the interaction had not yet occurred
-
-        print("AAAAAAAAAAAAA",zero_inds)
-
         # Get the predicted values based on our user/item vectors
         person_vec = predictions[0][person,:]
         pred = person_vec.dot(content_vecs).toarray()[0,zero_inds].reshape(-1)
-        
+
         # Get only the items that were originally zero
         # Select all ratings from the MF prediction for this user that originally had no iteraction
         actual = test_set[:,person].toarray()[zero_inds,0].reshape(-1)
@@ -265,7 +254,8 @@ def calc_mean_auc(training_set, altered_persons, test_set):
         
         popularity_auc.append(auc_score(pop, actual)) # Calculate AUC using most popular and score
         # End users iteration
-    
+
+    print("Exiting calc_mean_auc\n")
     return float('%.3f'%np.mean(store_auc)), float('%.3f'%np.mean(popularity_auc))
 
 def main():
@@ -274,17 +264,15 @@ def main():
     #Als()
 
     # get items similar to specific item
-    product_id = 1862
-    find_similar_products(product_id,30)  
+    product_id = 14865
+    find_similar_products(product_id,10)  
    
     # Create recommendations for person with specific id
-    person_id = 89371
-    recommend(person_id)
+    user_id = 134
+    recommend(user_id)
     
-    #content_train, content_test, content_persons_altered = make_train(pct_test = 0.2)
-    #calc_mean_auc(content_train, content_persons_altered, content_test)
-    #event_strength_to_rating()
-    #train()
+    content_train, content_test, content_persons_altered = make_train(pct_test = 0.2)
+    print(calc_mean_auc(content_train, content_persons_altered, content_test)
 
 if __name__ == "__main__":
 	main()
